b92e4cfc23b9cae4eebb65d44ae7bcbee39dbf10
mrsargio <mrsargio5@gmail.com>
2025-10-05 22:25:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ load_dotenv()
 
 class Config:
     # Telegram API Configuration
-    API_ID = int(os.getenv('API_ID', 0))
-    API_HASH = os.getenv('API_HASH', '')
-    BOT_TOKEN = os.getenv('BOT_TOKEN', '')
+    API_ID = int(os.getenv('24250238', 0))
+    API_HASH = os.getenv('cb3f118ce5553dc140127647edcf3720', '')
+    BOT_TOKEN = os.getenv('6047785902:AAE59KTfmhRvF8sUSYIzl9wcGnm4FLXiWDk', '')
     
     # Bot Configuration
-    ADMIN_IDS = [int(x) for x in os.getenv('ADMIN_IDS', '').split(',') if x]
+    ADMIN_IDS = [int(x) for x in os.getenv('6175650047', '').split(',') if x]
